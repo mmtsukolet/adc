928868b714fe58f05a43d6e3f45a264a87225cd7
mm <alasmichellemarie@gmail.com>
2016-11-22 14:48:55 +0800

    found potential api to be used

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
   
   	var rand_number = Math.floor(Math.random() * 50) + 1;
-	var url = 'http://jsonplaceholder.typicode.com/posts/{rand_number}'.replace('{rand_number}', rand_number);
+	// var url = 'http://jsonplaceholder.typicode.com/posts/{rand_number}'.replace('{rand_number}', rand_number);
+	// var url = 'http://quotes.rest/bible/verse.json';
+	// var url = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand';
 	
+	var url = "https://api.chucknorris.io/jokes/random";
 	$.ajax({
+
 	  url: url,
-	  method: 'GET'
+	  method: 'GET',
+	  error: function(err) {
+  		var h1 = '<h1></h1>';
+  		var body = '<p>Those who say truth is stranger than fiction have wasted their time on poorly written fiction.</p>';
+	  	$('.holder').html(h1 + body);
+	  }
+
 	}).done( function(msg) {
-		var h1 = '<h1>' + msg.title + '</h1>'; 
-  		var body = '<p>' + msg.body + '</p>';
+  		
+  		var h1 = '<img src="'+msg.icon_url +'" /> Kwotspirasyon by: Chuck Norris' + '<br/>';
+  		var body = '<p>' + msg.value + '</p>';
   		$('.holder').html(h1 + body);
   	});
-
 
 });
