97a16a067c40e4aafa6fab6afa6b6a49a52f236d
Mark Valles <mkvalles@gmail.com>
2016-11-26 14:14:37 +0800

    Remove backend

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   
   	var rand_number = Math.floor(Math.random() * 50) + 1;
-	var url = "https://api.chucknorris.io/jokes/random";
+	var url = "http://localhost/adc/api/quotes/random";
 
 	$.ajax({
 
 	  url: url,
 	  method: 'GET',
+		type: "json",
 	  error: function(err) {
-  		var h1 = '<h1></h1>';
-  		var body = '<p>Those who say truth is stranger than fiction have wasted their time on poorly written fiction.</p>';
-	  	$('.holder').html(h1 + body);
+  		//var h1 = '<h1></h1>';
+  		//var body = '<p>Those who say truth is stranger than fiction have wasted their time on poorly written fiction.</p>';
+	  	$('.holder').html("loading...");
 	  }
 
-	}).done( function(msg) {
+	}).done( function(json) {
+		$('.holder').html(json.text);
+		console.log(json.text)
+		return;
   		
   		var link = 'http://img-9gag-fun.9cache.com/photo/aeYg2Bq_700b_v2.jpg'; //'<a href=' + msg.url + '> Kwotspirasyon by: Chuck Norris </a>';
   		var h1 = '<img src="'+ msg.icon_url +'" /> ' + link + '<br/>';
